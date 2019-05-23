499e5a005ea9991af4dff8283eca9f526f0cce62
Relun <claudia.araya.ro@gmail.com>
2019-05-22 21:25:52 -0400

    modal

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ const showElements = (data, year) => {
                 .then(function(myTitle) {
                     console.log(myTitle);
                cardHTML +=
-               `<div class="card s12 m12 l3 xl3" style="max-width: 540px;">
+               `<div class="card s12 m12 l3 xl3" style="max-width: 540px;"data-toggle="modal" data-target="#${myTitle.imdbID}" >
                <div class="row no-gutters">
                  <div class="col-md-6">
                    <img src="${myTitle.Poster}" class="card-img" alt="..."onerror="this.onerror=null;this.src='img/descarga.png';"/>
@@ -60,10 +60,33 @@ const showElements = (data, year) => {
                      <p class="card-text"> <small class="text-muted">Director: </small> ${myTitle.Director}</p>
                      <p class="card-text"><small class="text-muted"> Runtime: </small> ${myTitle.Runtime}</p>
                      <p class="card-text"><small class="text-muted">Released: </small>${myTitle.Released}</p>
+
                    </div>
                  </div>
                </div>
-             </div>` 
+             </div>
+
+             <div class="modal fade" id="${myTitle.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+               <div class="modal-dialog modal-dialog-centered" role="document">
+                 <div class="modal-content">
+                   <div class="modal-body">
+                   <img class='modal-img' src="${myTitle.Poster}"/>
+                   <h5> ${myTitle.Title}</h5>
+                   <p>Sinopsis: ${myTitle.Plot}</p>
+                   <p>Actores: ${myTitle.Actors}</p>
+                   <p>Subgénero: ${myTitle.Genre}</p>
+                   <p>País: ${myTitle.Country}</p>
+                   <p>Premios: ${myTitle.Awards}</p>  
+                   <p>Puntuación: ${myTitle.Ratings[0].Value}</p> 
+                   <p>Website:<a href= "${myTitle.Website}">${myTitle.Website}</a> </p>  
+                   <button type="button" class="btn btn-dark" data-dismiss="modal">Cerrar</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+             
+             ` 
              containerResult.innerHTML = cardHTML;             
    
         })
